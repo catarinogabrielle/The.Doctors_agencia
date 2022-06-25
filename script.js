d1ee2e60492820c7b0366832ff90366ced4be6d2
Gabrielle De Castro Santos Catarino <54675970+catarinogabrielle@users.noreply.github.com>
2022-06-25 01:01:10 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.content', {
+    rotate: { x: 0, y: 80, z: 0 },
+    duration: 2000
+});
+
+sr.reveal('.about-img', {
+    rotate: { x: 0, y: 80, z: 0 },
+    duration: 1000
+});
+
+sr.reveal('.about-text h2', { duration: 1000 });
+
+sr.reveal('.about-text h5', { duration: 1000 });
+
+sr.reveal('.about-text p', { duration: 1000 });
+
+sr.reveal('.box', {
+    rotate: { x: 100, y: 0, z: 0 },
+    duration: 1200
+});
+
+sr.reveal('.more-content', { duration: 1000 });
+
+sr.reveal('#content-footer', {
+    rotate: { x: 0, y: 80, z: 0 },
+    duration: 1000
+});
